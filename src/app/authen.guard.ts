1643bfe8aa52667b9ae160db89a07240e8853b97
up1 <somkiat.p@gmail.com>
2022-04-29 16:27:45 +0700

    Create guard

---
+++ src/app/authen.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenGuard implements CanActivate {
+
+  constructor(private route: Router){}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    // TODO :: check data from request
+    // 1. Not login
+    this.route.navigate(["login"]);
+    // 2. Logged in
+    return true;
+
+  }
+
+}
